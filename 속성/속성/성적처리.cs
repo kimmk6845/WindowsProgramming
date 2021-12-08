4e087af8c2b3d1134fb6444fb1ff8c29b052eeb5
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2021-12-08 15:59:52 +0900

    Add files via upload

---
+++ 속성/속성/성적처리.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 속성
+{
+    class 성적처리
+    {
+        public class seongjuk
+        {
+            private string _hakbun;
+            private string _name;
+            private int _kor;
+            private int _mat;
+
+            private int _tot;
+            private int _aver;
+
+            public seongjuk(string h, string n, int k, int m)
+            {
+                _hakbun = h;
+                _name = n;
+                _kor = k;
+                _mat = m;
+
+                _tot = sum(_kor, _mat);
+                _aver = average(_kor, _mat);
+            }
+
+
+            public string Hakbun
+            {
+                get { return _hakbun; }
+                set { _hakbun = value; }
+            }
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+            public int Kor
+            {
+                get { return _kor; }
+                set { _kor = value; }
+            }
+            public int Mat
+            {
+                get { return _mat; }
+                set { _mat = value; }
+            }
+            public int Tot { get { return _tot; } }
+            public int Aver { get { return _aver; } }
+
+            public int sum(int k, int m)
+            {
+                return (k + m);
+            }
+            public int average(int k, int m)
+            {
+                int a = sum(k, m);
+                return a / 2;
+            }
+        }
+
+        class 속성_성적처리
+        {
+            static void Main(string[] args)
+            {
+                string hakbun = "";
+                string name = "";
+                int kor = 0, mat = 0;
+
+                Console.Write("학번 입력 : ");
+                hakbun = Console.ReadLine();
+                Console.Write("이름 입력 : ");
+                name = Console.ReadLine();
+                Console.Write("국어 입력 : ");
+                kor = Convert.ToInt32(Console.ReadLine());
+                Console.Write("수학 입력 : ");
+                mat = Convert.ToInt32(Console.ReadLine());
+
+                seongjuk jumsu = new seongjuk(hakbun, name, kor, mat);
+
+                Print_Out(jumsu.Hakbun, jumsu.Name, jumsu.Kor, jumsu.Mat, jumsu.Tot, jumsu.Aver);
+            }
+
+            private static void Print_Out(string h, string n, int k, int m, int t, int a)
+            {
+                Console.WriteLine("===============================================");
+                Console.WriteLine("학번\t이름\t국어\t수학\t총점\t평균");
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", h, n, k, m, t, a);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 속성/속성/택시요금.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 속성
+{
+    public class taxi
+    {
+        private int _distance;
+
+        private int _pay;
+        private int _buga;
+        private int _tot;
+
+        public taxi(int d)  //생성자
+        {                   //메인에서 거리만 입력받으므로 나머지는 메소드 처리
+            _distance = d;
+
+            _pay = Pay1(_distance);
+            _buga = Buga1(_pay);
+            _tot = Tot1(_pay, _buga);
+        }
+
+        public int Distance { get { return _distance; } set { _distance = value; } }
+        public int Pay { get { return _pay; } }
+        public int Buga { get { return _buga; } }
+        public int Tot { get { return _tot; } }
+
+        public int Pay1(int d)
+        {
+            return d * 2000;
+        }
+        public int Buga1(int p)
+        {
+            return p / 10;
+        }
+        public int Tot1(int p, int b)
+        {
+            return p + b;
+        }
+    }
+    class 속성_택시요금
+    {
+        static void Main(string[] args)
+        {
+            int distance = 0;
+            Console.Write("택시로 이동할 거리(KM)를 정수로 입력하세요 : ");
+            distance = Convert.ToInt16(Console.ReadLine());
+
+            taxi t = new taxi(distance);
+
+            Print_Out(t.Distance, t.Pay, t.Buga, t.Tot);
+        }
+
+        private static void Print_Out(int d, int p, int b, int t)
+        {
+            Console.WriteLine("========================================");
+            Console.WriteLine("이동거리  택시 요금  부가세  요금 합계");
+            Console.WriteLine("{0}KM      {1}원    {2}원  {3}원", d, p, b, t);
+        }
+    }
+
+}
