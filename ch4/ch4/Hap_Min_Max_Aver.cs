6e9532927c3911993431473b0e15cf239a8c4f64
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2021-12-08 15:54:21 +0900

    Add files via upload

---
+++ ch4/ch4/Hap_Min_Max_Aver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch4
+{
+    class Hap_Min_Max_Aver
+    {
+        static void Main(string[] args)
+        {
+            int MAX = 0;
+            int MIN = 10;
+            int HAP = 0;
+            int AVER = 0;
+            int[] su = new int[7] { 1, 3, 5, 7, 2, 4, 6 };
+
+            for(int i=0;i<su.Length;i++)        //합
+            {
+                HAP += su[i];
+            }
+
+            for(int i=0;i<su.Length;i++)        //최대
+            {
+                if (MAX < su[i])
+                    MAX = su[i];
+            }
+
+            for(int i=0;i<su.Length;i++)        //최소
+            {
+                if (MIN > su[i])
+                    MIN = su[i];
+            }
+
+            AVER = HAP / su.Length;             //평균
+
+            Console.WriteLine("합계 = {0}, 최댓값 = {1}, 최솟값 = {2}, 평균 = {3}",HAP,MAX,MIN,AVER);
+
+            Console.ReadKey();
+        }
+    }
+}
