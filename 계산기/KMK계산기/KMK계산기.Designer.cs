71f64d90d4c9ce601715e6514fa2a880db4523a3
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2021-12-08 15:59:32 +0900

    Add files via upload

---
+++ 계산기/KMK계산기/KMK계산기.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace KMK계산기
+{
+    partial class KMK계산기
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(70, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(111, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 25);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(152, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 25);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_oper_click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(29, 101);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(35, 25);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(70, 101);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(35, 25);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(111, 101);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(35, 25);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(152, 101);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(35, 25);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "-";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_oper_click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(29, 132);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(35, 25);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "7";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(70, 132);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(35, 25);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "8";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(111, 132);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(35, 25);
+            this.button11.TabIndex = 1;
+            this.button11.Text = "9";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(152, 132);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(35, 25);
+            this.button12.TabIndex = 1;
+            this.button12.Text = "*";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_oper_click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(29, 163);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(35, 25);
+            this.button13.TabIndex = 1;
+            this.button13.Text = "0";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_num_click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(70, 163);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(35, 25);
+            this.button14.TabIndex = 1;
+            this.button14.Text = "C";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_clear_click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(111, 163);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(35, 25);
+            this.button15.TabIndex = 1;
+            this.button15.Text = "=";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_calc_click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(152, 163);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(35, 25);
+            this.button16.TabIndex = 1;
+            this.button16.Text = "/";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_oper_click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(193, 70);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(35, 25);
+            this.button17.TabIndex = 1;
+            this.button17.Text = "%";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_oper_click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(193, 101);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(35, 25);
+            this.button18.TabIndex = 1;
+            this.button18.Text = "^";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_oper_click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(193, 132);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(35, 25);
+            this.button19.TabIndex = 1;
+            this.button19.Text = "!";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_oper_click);
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(193, 163);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(35, 25);
+            this.button20.TabIndex = 1;
+            this.button20.Text = "√";
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bt_oper_click);
+            // 
+            // KMK계산기
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 211);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "KMK계산기";
+            this.Text = "KMK계산기";
+            this.Load += new System.EventHandler(this.KMK계산기_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+    }
+}
+
---
+++ 계산기/KMK계산기/KMK계산기.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KMK계산기
+{
+    public partial class KMK계산기 : Form
+    {
+        // 계산기에서 사용할 변수 선언
+        string output = "";      // 임시 출력 할 변수
+        char oper;               // 연산자 저장할 변수 + , - , * , /
+        double result;          // 결과 값 저장할 변수
+        string str = "";        //중위표기식 (사용자 입력)
+        string postFix = "";        //후위표기식
+
+        public KMK계산기()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_num_click(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+            output += btn.Text;
+            textBox1.Text = output;
+            str += btn.Text;
+        }
+
+        private void bt_oper_click(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+            output += btn.Text;
+            textBox1.Text = output;
+            oper = btn.Text[0];
+            str += oper;
+        }
+
+        private void bt_clear_click(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+            output = "";
+            result = 0.0;
+            str = "";
+        }
+
+        private void KMK계산기_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = output;
+        }
+
+        private void bt_calc_click(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            postFix = InToPost(str);        //중위 표기식 -> 후위 표기식
+            result = Cal(postFix);          //후위표기식 계산
+            output += btn.Text + result.ToString();         //=과 계산결과를 임시출력 변수에 저장
+            textBox1.Text = output;         //결과 출력
+            str = "";               //저장되어있던 중위표기식 초기화 시킨 후
+            str += result.ToString();       //결과 값을 저장(다음 계산을 위함)
+        }
+        private double Cal(string postfix)        //스택 이용한 후위표기식 계산
+        {
+            double op1, op2;        //계산할 피연산자
+            Stack s = new Stack(100);       //크기가 100인 스택 생성,후위표기식 계산을 위함
+
+            for (int i = 0; i <= postfix.Length - 1; i++)
+            {
+                if (postfix[i] != '+' && postfix[i] != '-' &&
+                    postfix[i] != '*' && postfix[i] != '/' &&
+                    postfix[i] != '%' && postfix[i] != '^' &&
+                    postfix[i] != '!' && postfix[i] != '√')
+                {       //입력이 피연산자
+                    s.push(postfix[i].ToString());      //스택에 값을 넣음
+                }
+                else
+                {       //연산자이면
+                    switch (postfix[i])
+                    {                           //피연산자를 pop해서 변수에 저장하고 각 연산자에 해당하는 연산
+                        case '+':               //결과값을 다시 스택에 넣음(push)
+                            op2 = Convert.ToDouble(s.pop());
+                            op1 = Convert.ToDouble(s.pop());
+                            s.push(Convert.ToString(op1 + op2)); break;
+                        case '-':
+                            op2 = Convert.ToDouble(s.pop());
+                            op1 = Convert.ToDouble(s.pop());
+                            s.push(Convert.ToString(op1 - op2)); break;
+                        case '*':
+                            op2 = Convert.ToDouble(s.pop());
+                            op1 = Convert.ToDouble(s.pop());
+                            s.push(Convert.ToString(op1 * op2)); break;
+                        case '/':
+                            op2 = Convert.ToDouble(s.pop());
+                            op1 = Convert.ToDouble(s.pop());
+                            s.push(Convert.ToString(op1 / op2)); break;
+                        case '%':
+                            op2 = Convert.ToDouble(s.pop());
+                            op1 = Convert.ToDouble(s.pop());
+                            s.push(Convert.ToString(op1 % op2)); break;
+                        case '^':
+                            op2 = Convert.ToDouble(s.pop());
+                            op1 = Convert.ToDouble(s.pop());
+                            double a = op1;
+                            for (int j = 1; j < op2; j++)
+                                op1 *= a;
+                            op2 = op1;
+                            s.push(Convert.ToString(op2)); break;
+                        case '!':
+                            op2 = Convert.ToDouble(s.pop());
+                            int n = 1;
+                            for (int j = 1; j <= (int)op2; j++)
+                                n *= j;
+                            s.push(Convert.ToString(n)); break;
+                        case '√':
+                            op2 = Convert.ToDouble(s.pop());
+                            double num = Math.Sqrt(op2);
+                            s.push(Convert.ToString(num)); break;
+                    }
+                }
+            }
+            return Convert.ToDouble(s.peek());      //모든 연산이 끝나면 스택의 top값인 연산결과값을 반환함
+        }
+        private int prec(char op)       //연산자 우선순위
+        {
+            int pri = 0;
+            switch (op)         //우선순위 : +,- < *,/,%,^ < !,√
+            {
+                case '+': case '-': pri = 1; break;
+                case '*': case '/': case '%': case '^': pri = 2; break;
+                case '!': case '√': pri = 3; break;
+            }
+            return pri;
+        }
+
+        private string InToPost(string infix)         //중위표기 -> 후위표기
+        {
+            Stack s = new Stack(100);       //크기가 100인 스택 생성
+            string postfix = "";            //후위 표기식을 저장할 변수
+
+            for (int i = 0; i < infix.Length; i++)
+            {
+                switch (infix[i])
+                {
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '%':
+                    case '^':
+                    case '!':
+                        while (!s.is_empty() && (prec(infix[i]) <= prec(Convert.ToChar(s.peek()))))         //스택에 있는 연산자 우선순위가 더 크거나 같으면
+                            postfix += s.pop();             //pop해서 변수에 저장
+                        s.push(infix[i].ToString());        //스택에 연산자 넣음
+                        break;
+                    default:        //피연산자
+                        postfix += infix[i];        //피연산자일경우 변수에 바로 저장
+                        break;
+                }
+            }
+            while (!s.is_empty())       //스택에 저장된 연산자들 pop
+                postfix += s.pop();
+
+            return postfix;         //후위표기식 반환
+        }
+    }
+
+    public class Stack      //스택 클래스
+    {
+        string[] stack;
+        int top;
+
+        public Stack(int size)      //생성자
+        {
+            stack = new string[size];
+            top = -1;
+        }
+        public bool is_empty()      //스택이 비어있는지 확인하는 메소드
+        {
+            if (top == -1) return true;
+            else return false;
+        }
+
+        public string peek()        //스택의 top값 반환 메소드
+        {
+            string o = stack[top];
+            return o;
+        }
+
+        public string pop()     //스택의 top이 빠지며 값을 반환하는 메소드
+        {
+            string o = stack[top];
+            stack[top] = null;
+            top--;
+            return o;
+        }
+
+        public void push(string Stack)      //스택에 값을 넣는 메소드
+        {
+            top++;
+            stack[top] = Stack;
+        }
+    }
+}
