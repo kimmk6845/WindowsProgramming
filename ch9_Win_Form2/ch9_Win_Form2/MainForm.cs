b93f7742be42097913ca994489d6205b476f7aca
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2021-12-08 15:56:16 +0900

    Add files via upload

---
+++ ch9_Win_Form2/ch9_Win_Form2/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch9_Win_Form2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOutput_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = txtName.Text;
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            txtName.Text = null;
+            txtResult.Text = null;
+        }
+    }
+}
